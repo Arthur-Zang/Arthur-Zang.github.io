fde3ce1463155b52b9553c5035f5358cf043a624
Arthur Zang <galaxionman@gmail.com>
2023-09-01 01:33:20 -0400

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Arthur-Zang.github.io",
+  base: "/Portfolio-Website/",
   plugins: [react()],
 })
